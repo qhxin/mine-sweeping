935a072ff19acbf19ab190a06fe0f5bdaecfbc2e
罗素 <luosu@mikecrm.com>
2016-12-12 19:21:30 +0800

    m

--- www/SaoLei/js/index.js
+++ www/SaoLei/js/index.js
@@ -1,35 +1,78 @@
 (function(){
     'use strict';
 
-    var Main = React.createClass({
-
+    let Box = React.createClass({
         getInitialState: function(){
+            let w = this.props.boxWidth,
+                h = this.props.boxHeight;
+
             return {
-                'level': 1
+                num: parseInt(h*w/400, 10)
             };
         },
+
         render: function(){
-            let width, height;
-            if(this.state.level==2){
-                width = 500;
-                height = 400;
-            }else
-            if(this.state.level==3){
-                width = 600;
-                height = 500;
-            }else{
-                width = 300;
-                height = 400;
+
+            let lei = [];
+
+            for(let i=0; i< this.state.num; i++){
+                lei.push(<div className="sl-lei"></div>);
+            }
+
+            return (
+                <div className="sl-box" style={{width: this.state.w, height: this.state.h}}>
+                {lei}
+                </div>
+            );
+        }
+
+    });
+
+    let Main = React.createClass({
+        getInitialState: function(){
+            return {
+                level: 1
+            };
+        },
+        handlerSelectLevel: function(e){
+
+            this.setState({
+                level: parseInt(e.target.value, 10)
+            })
+        },
+        render: function(){
+
+            let level_map = {
+                    1: { n:'菜鸟', w:300, h:400},
+                    2: { n:'高手', w:500, h:400},
+                    3: { n:'传奇', w:600, h:500}
+                };
+
+            let b_width = level_map[this.state.level].w,
+                b_height = level_map[this.state.level].h;
+
+            let width = b_width+8,
+                height = b_height+40;
+
+            let levels_select_opt = [];
+            for(let le in level_map){
+                if(level_map.hasOwnProperty(le)){
+                    levels_select_opt.push(<option value={le}>{level_map[le].n}</option>);
+                }
             }
 
             return (
                 <div className="sl-main" style={{ 'width': width, 'height': height, 'margin-top': -(height/2),'margin-left':-(width/2) }}>
-                    <div className="sl-title"></div>
+                    <div className="sl-title">
+                        <span>扫雷</span>
+                        <span> | </span>
+                        <select className="sl-level-select" defaultValue={this.state.level} onChange={this.handlerSelectLevel}>{levels_select_opt}</select>
+                    </div>
+                    <Box boxWidth={b_width} boxHeight={b_height}/>
                 </div>
             );
         }
     });
 
-    var mainEle = document.getElementById('Main');
-    ReactDOM.render(<Main/>, mainEle);
+    ReactDOM.render(<Main/>, document.getElementById('Main'));
 })();
